631cd887d2daf80d015d68e45bac84e017c6a517
Krzysztof Dudek <krzysztofdudek1232@gmail.com>
2026-08-28 10:59:24 +0200

    Remove .vscode and update .gitignore

---
+++ src/cpp_minimal_subscriber.cpp
@@ -0,0 +1,43 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using std::placeholders::_1;
+
+class MinimalCppSubscriber : public rclcpp::Node
+{
+public:
+    MinimalCppSubscriber() : Node("cpp_minimal_subscriber")
+    {
+        subscriber_ = create_subscription<std_msgs::msg::String>
+        (
+            "/cpp_example_topic",
+            10,
+            std::bind(
+                &MinimalCppSubscriber::topicCallback,
+                this,
+                _1
+            )
+        );
+    }
+
+    void topicCallback(const std_msgs::msg::String & msg) const
+    {
+        RCLCPP_INFO_STREAM(get_logger(),"Iheard: " << msg.data.c_str());
+    }
+private:
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber_;
+
+};
+
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc,argv);
+
+    auto minimal_cpp_subscriber_node = std::make_shared<MinimalCppSubscriber>();
+
+    rclcpp::spin(minimal_cpp_subscriber_node);
+
+    rclcpp::shutdown();
+
+    return 0;
+}
